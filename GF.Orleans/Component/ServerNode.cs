51c517c3038c58e70dfc20e36c4c9c2dac30dc19
lion <xiaolige945@vip.qq.com>
2016-06-23 03:38:51 +0800

    建立项目结构。

---
+++ GF.Orleans/Component/ServerNode.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Net;
+//using System.Text;
+//using CG.Unity.Common;
+
+//namespace GF.Server
+//{
+//    public class ServerNode<TDef> : Component<TDef> where TDef : DefNode, new()
+//    {
+//        //---------------------------------------------------------------------
+//        public ServerSuperSocket<DefSuperSocket> CoSuperSocket { get; private set; }
+
+//        //---------------------------------------------------------------------
+//        public override void init()
+//        {
+//            EbLog.Note("ServerNode.init()");
+
+//            var settings = EsEngine.Instance.Settings;
+
+//            if (settings.EnableCoSupersocket)
+//            {
+//                var et = EntityMgr.createEntity<EtSuperSocket>(null, Entity);
+//                CoSuperSocket = et.getComponent<ServerSuperSocket<DefSuperSocket>>();
+//            }
+//        }
+
+//        //---------------------------------------------------------------------
+//        public override void release()
+//        {
+//            EbLog.Note("ServerNode.release()");
+//        }
+
+//        //---------------------------------------------------------------------
+//        public override void update(float elapsed_tm)
+//        {
+//        }
+
+//        //---------------------------------------------------------------------
+//        public override void handleEvent(object sender, EntityEvent e)
+//        {
+//        }
+//    }
+//}
---
+++ GF.Orleans/Main/EsEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+//using CG.Unity.Common;
+
+namespace GF.Orleans
+{
+    public struct EsEngineSettings
+    {
+        public byte NodeType;
+        public string NodeTypeString;
+        public string ListenIp;
+        public int ListenPort;
+        public string RootEntityType;
+        public bool EnableCoSupersocket;
+        public string Log4NetConfigPath;
+    }
+
+    public interface IEsEngineListener
+    {
+        //---------------------------------------------------------------------
+        void init();
+
+        //---------------------------------------------------------------------
+        void release();
+    }
+
+    public sealed class EsEngine
+    {
+        //---------------------------------------------------------------------
+        static EsEngine mInstance;
+        IEsEngineListener mListener;
+
+        //---------------------------------------------------------------------
+        public static EsEngine Instance { get { return mInstance; } }
+        public EsEngineSettings Settings { get; private set; }
+
+        //---------------------------------------------------------------------
+        public EsEngine(ref EsEngineSettings settings, IEsEngineListener listener)
+        {
+        }
+    }
+}
